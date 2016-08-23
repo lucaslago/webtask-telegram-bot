c904c8cef907363c917bc5f02975a27158f066a0
Lucas Lago <lucasbenderlago@gmail.com>
2016-08-23 16:22:37 -0300

    remove unnecessary condition

--- src/app.js
+++ src/app.js
@@ -14,13 +14,13 @@ module.exports = (context, cb) => {
   const userText = context.data.message.text;
   const chat = context.data.message.chat.id;
 
-  if(userText && (/(^|\s)passado(\s|$)/.test(userText))) {
+  if(/(^|\s)passado(\s|$)/.test(userText)) {
     sendMessage(token, chat, '"Temos que parar de escrever tanto, está difícil de ver o passado." - Autor Desconhecido');
-  } else if(userText && (/(^|\s)(oi|ola|olá|hello|bom dia|Bom dia)(\s|$)/.test(userText))) {
+  } else if(/(^|\s)(oi|ola|olá|hello|bom dia|Bom dia)(\s|$)/.test(userText)) {
     sendMessage(token, chat, 'Olá Jovem!');
-  } else if(userText && (/(^|\s)(estilo|css|ensina css)(\s|$)/.test(userText))) {
+  } else if(/(^|\s)(estilo|css|ensina css)(\s|$)/.test(userText)) {
     sendMessage(token, chat, 'Eu tenho estilos!');
-  } else if(userText && (/(^|\s)(adeus|tchau|boa noite|flw)(\s|$)/.test(userText))) {
+  } else if(/(^|\s)(adeus|tchau|boa noite|flw)(\s|$)/.test(userText)) {
     sendMessage(token, chat, 'Há braços!');
   }
   cb(null, {status: 'ok'});
